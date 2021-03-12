cc94c1dbccb8125f53e7173aef53246741e2f27a
Sowa12 <vip.kuzneccov@mail.ru>
2021-03-13 01:20:21 +0500

    Модальное окно

---
+++ blog/resources/js/components/body.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div>
+            Услуги печати
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "body"
+    }
+</script>
+
+<style scoped>
+
+</style>
